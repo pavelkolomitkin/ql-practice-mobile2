abc459cbd7185943701a12836ad478469955355e
Pavel Kolomytkin <pavelkolomitkin@gmail.com>
2020-01-08 13:34:28 +0300

    add facebook login layout

--- screens/security/facebook-auth.js
+++ screens/security/facebook-auth.js
@@ -1,16 +1,39 @@
 import React, {Component} from 'react';
 
 import {Text, View} from 'react-native';
-// import Layout from '../../components/common/layout';
+import Layout from './layout';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as actions from '../../redux/actions/security';
+import { Button } from 'native-base';
 
-// import styles from './styles';
-
-export default class FacebookAuth extends Component {
+class FacebookAuth extends Component {
     render() {
         return (
-            <View>
-                <Text>Facebook Auth Screen</Text>
-            </View>
+            <Layout title="FaceBook Login">
+                <View style={{ flex: 1, flexDirection: 'column', alignContent: 'center', justifyContent: 'center' }}>
+                    <Button
+                        style={{ justifyContent: 'center' }}
+                        active
+                        info>
+                        <Text style={{ color: '#fff', textTransform: 'uppercase'  }}>FB Login</Text>
+                    </Button>
+                </View>
+            </Layout>
         );
     }
 }
+
+const mapStateToProps = (state) => {
+    return {
+
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        actions: bindActionCreators(actions, dispatch)
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(FacebookAuth);
